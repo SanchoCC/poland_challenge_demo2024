e43a7156ccd4cbfcaaa53a7f95ebc4864349099d
SanchoCC <sanchocat1313@gmail.com>
2024-11-07 13:54:41 +0100

    Revert "nsfnnfwfniwqnf"
    This reverts commit 718fc120d0af5dc59a6047da554b00ad1026a9be.

--- libc/ann.cpp
+++ libc/ann.cpp
@@ -8,7 +8,6 @@
 #include "ann/builder.hpp"
 #include "ann/nsg/nsg.hpp"
 #include "ann/searcher/graph_searcher.hpp"
-#include "ann/hnsw/hnsw.hpp"
 
 
 
@@ -28,32 +27,29 @@ bool is_load = 0;
 
 
 using IndexNSG = ann::NSG;
-using IndexHNSW = ann::HNSW<ann::FP32Quantizer<ann::Metric::IP>>;
 std::unique_ptr<ann::GraphSearcherBase> searcher;
 
 void *ann_init(int K_features, int R, const char *metric){
-    ann_R = R;
-    ann_L = R + 50;
+    ann_R = R + 80;
+    ann_L = R + 20;
     std::string metricS(metric);
-    /*IndexNSG *vidx = new IndexNSG(K_features, metricS, ann_R, ann_L);
+    IndexNSG *vidx = new IndexNSG(K_features, metricS, ann_R, ann_L);
     vidx->nndescent_iter = nndescent_iter;
     vidx->GK = nndescent_GK;
     vidx->nndescent_S = nndescent_S;
     vidx->nndescent_R = nndescent_R;
     vidx->nndescent_L = nndescent_L;
-    return (void *)vidx;*/
-
-    return (void*)ann::create_hnsw(metricS, "FP32", K_features, ann_R, ann_L).release();
+    return (void *)vidx;
 }
 
 void ann_free(void *ptr){
-    IndexHNSW* vidx = (ann::HNSW<ann::FP32Quantizer<ann::Metric::IP>>*) ptr;
+    IndexNSG *vidx = (IndexNSG *) ptr;
     delete vidx;
 }
 
 void ann_add(void *ptr, int n, float * x,  const char *store){
     
-    IndexHNSW*vidx = (IndexHNSW*)ptr;
+    IndexNSG *vidx = (IndexNSG *)ptr;
     if(!is_load){
         vidx->Build(x, n);
     }
@@ -64,10 +60,10 @@ void ann_add(void *ptr, int n, float * x,  const char *store){
     }
 
 
-    searcher = std::move(ann::create_searcher(std::move(vidx->final_graph), "IP", "FP32"));
+    searcher = std::move(ann::create_searcher(std::move(vidx->final_graph), vidx->metric, "SQ8U"));
 
 
-    searcher->SetData(x, n, vidx->quant.dim());
+    searcher->SetData(x, n, vidx->d);
 }
 
 void set_ann_ef(void *ptr, int ann_ef){
@@ -78,11 +74,11 @@ void set_ann_ef(void *ptr, int ann_ef){
 
 void ann_search(void *ptr, int n, const float* x, int k, float* distances,
                 int32_t* labels, int num_p){
-    IndexHNSW*vidx = (IndexHNSW*)ptr;
+    IndexNSG *vidx = (IndexNSG *)ptr;
     // 调用c++函数
 #pragma omp parallel for num_threads(num_p)
     for (int i = 0; i < n; ++i) {
-        size_t offset = i * vidx->quant.dim();
+        size_t offset = i * vidx->d;
         searcher->Search(x + offset, k, labels + i * k);
     }
 }
@@ -90,14 +86,14 @@ void ann_search(void *ptr, int n, const float* x, int k, float* distances,
 
 
 void ann_load(void *ptr, const char *path){
-    IndexHNSW*vidx = (IndexHNSW*)ptr;
+    IndexNSG *vidx = (IndexNSG *)ptr;
     vidx->final_graph.load(std::string(path));
     is_load = 1;
     
 }
 
 void ann_save(void *ptr, const char *path){
-    IndexHNSW*vidx = (IndexHNSW*)ptr;
+    IndexNSG *vidx = (IndexNSG *)ptr;
     vidx->final_graph.save(std::string(path));
 }
 
