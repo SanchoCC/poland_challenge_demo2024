18518f2620c61ae4d95e78288ccaca5404378cce
SanchoCC <sanchocat1313@gmail.com>
2024-11-17 10:19:32 +0100

    Revert "avx512 try"
    This reverts commit 6473ffa80e2352cd32bce4b2d5cf2c25f65fed6e.

--- ann/third_party/helpa/helpa/ref/dot_ref.hpp
+++ ann/third_party/helpa/helpa/ref/dot_ref.hpp
@@ -1,35 +1,16 @@
 #pragma once
 
 #include "ann/third_party/helpa/helpa/types.hpp"
-#include "immintrin.h"
 
 namespace helpa {
 
-inline float reduce_add_f32x16(__m512 v) {
-    __m256 low = _mm512_castps512_ps256(v);
-    __m256 high = _mm512_extractf32x8_ps(v, 1);
-    __m256 sum = _mm256_add_ps(low, high);
-    sum = _mm256_hadd_ps(sum, sum);
-    sum = _mm256_hadd_ps(sum, sum);
-    return _mm_cvtss_f32(_mm256_castps256_ps128(sum));
-}
-
 inline float dot_fp32_fp32_ref(const float *x, const float *y,
                                const int32_t d) {
-    __m512 sum = _mm512_setzero_ps();
-    int aligned_d = d & ~15;
-    for (int i = 0; i < aligned_d; i += 16) {
-        auto xx = _mm512_loadu_ps(x + i);
-        auto yy = _mm512_loadu_ps(y + i);
-        sum = _mm512_fmadd_ps(xx, yy, sum);
-    }
-
-    float tail_sum = 0.0f;
-    for (int i = aligned_d; i < d; ++i) {
-        tail_sum += x[i] * y[i];
-    }
-
-    return -reduce_add_f32x16(sum) - tail_sum;
+  auto ans = 0.0f;
+  for (int32_t i = 0; i < d; ++i) {
+    ans += x[i] * y[i];
+  }
+  return -ans;
 }
 
 inline float dot_fp32_fp16_ref(const float *x, const fp16 *y, const int32_t d) {
