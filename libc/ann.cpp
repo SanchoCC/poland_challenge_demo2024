c255af2ad647d7676d9845da013f30740245b3bf
SanchoCC <sanchocat1313@gmail.com>
2024-11-18 19:22:48 +0100

    try reduce dim

--- libc/ann.cpp
+++ libc/ann.cpp
@@ -11,8 +11,8 @@
 
 
 
-int nndescent_iter = 22;
-int nndescent_GK = 150;
+int nndescent_iter = 20;
+int nndescent_GK = 170;
 int nndescent_S = 10;
 int nndescent_R = 100;
 int nndescent_L = 200;
@@ -23,23 +23,98 @@ int ann_L = 100;
 
 bool is_load = 0;
 
-
-
+int original_dim = 0;
+int reduced_dim = 128;
 
 using IndexNSG = ann::NSG;
 std::unique_ptr<ann::GraphSearcherBase> searcher;
 
-void *ann_init(int K_features, int R, const char *metric){
-    ann_R = R + 40;
+void reduce_dim_PCA(const float* data, int n, int d, int target_dim, float*& reduced_data) {
+    std::vector<float> mean(d, 0.0f);
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < d; ++j) {
+            mean[j] += data[i * d + j];
+        }
+    }
+    for (int j = 0; j < d; ++j) {
+        mean[j] /= n;
+    }
+
+    std::vector<float> centered_data(n * d);
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < d; ++j) {
+            centered_data[i * d + j] = data[i * d + j] - mean[j];
+        }
+    }
+
+    std::vector<float> covariance(d * d, 0.0f);
+    for (int i = 0; i < d; ++i) {
+        for (int j = 0; j <= i; ++j) {
+            float sum = 0.0f;
+            for (int k = 0; k < n; ++k) {
+                sum += centered_data[k * d + i] * centered_data[k * d + j];
+            }
+            covariance[i * d + j] = sum / (n - 1);
+            covariance[j * d + i] = covariance[i * d + j];
+        }
+    }
+
+    std::vector<float> eigenvalues(d, 0.0f);
+    std::vector<float> eigenvectors(d * d, 0.0f);
+    for (int i = 0; i < d; ++i) eigenvectors[i * d + i] = 1.0f;
+
+    for (int iter = 0; iter < 100; ++iter) {
+        int p = 0, q = 1;
+        float max_val = fabs(covariance[p * d + q]);
+        for (int i = 0; i < d; ++i) {
+            for (int j = i + 1; j < d; ++j) {
+                if (fabs(covariance[i * d + j]) > max_val) {
+                    max_val = fabs(covariance[i * d + j]);
+                    p = i;
+                    q = j;
+                }
+            }
+        }
+        if (max_val < 1e-6) break;
+
+        float phi = 0.5 * atan2(2 * covariance[p * d + q], covariance[p * d + p] - covariance[q * d + q]);
+        float cos_phi = cos(phi);
+        float sin_phi = sin(phi);
+
+        for (int i = 0; i < d; ++i) {
+            float temp_p = cos_phi * eigenvectors[i * d + p] - sin_phi * eigenvectors[i * d + q];
+            float temp_q = sin_phi * eigenvectors[i * d + p] + cos_phi * eigenvectors[i * d + q];
+            eigenvectors[i * d + p] = temp_p;
+            eigenvectors[i * d + q] = temp_q;
+        }
+    }
+
+    reduced_data = new float[n * target_dim];
+    for (int i = 0; i < n; ++i) {
+        for (int k = 0; k < target_dim; ++k) {
+            float sum = 0.0f;
+            for (int j = 0; j < d; ++j) {
+                sum += centered_data[i * d + j] * eigenvectors[j * d + k];
+            }
+            reduced_data[i * target_dim + k] = sum;
+        }
+    }
+}
+
+void* ann_init(int K_features, int R, const char* metric) {
+    ann_R = R + 30;
     ann_L = R + 50;
+    original_dim = K_features;
+
     std::string metricS(metric);
-    IndexNSG *vidx = new IndexNSG(K_features, metricS, ann_R, ann_L);
+    IndexNSG* vidx = new IndexNSG(reduced_dim, metricS, ann_R, ann_L);
+
     vidx->nndescent_iter = nndescent_iter;
     vidx->GK = nndescent_GK;
     vidx->nndescent_S = nndescent_S;
     vidx->nndescent_R = nndescent_R;
     vidx->nndescent_L = nndescent_L;
-    return (void *)vidx;
+    return (void*)vidx;
 }
 
 void ann_free(void *ptr){
@@ -47,23 +122,24 @@ void ann_free(void *ptr){
     delete vidx;
 }
 
-void ann_add(void *ptr, int n, float * x,  const char *store){
-    
-    IndexNSG *vidx = (IndexNSG *)ptr;
-    if(!is_load){
-        vidx->Build(x, n);
+void ann_add(void* ptr, int n, float* x, const char* store) {
+    IndexNSG* vidx = (IndexNSG*)ptr;
+
+    float* reduced_data = nullptr;
+    reduce_dim_PCA(x, n, original_dim, reduced_dim, reduced_data);
+
+    if (!is_load) {
+        vidx->Build(reduced_data, n);
     }
-    
 
     if (store != NULL) {
         vidx->final_graph.save(std::string(store));
     }
 
-
     searcher = std::move(ann::create_searcher(std::move(vidx->final_graph), vidx->metric, "SQ8U"));
+    searcher->SetData(reduced_data, n, reduced_dim);
 
-
-    searcher->SetData(x, n, vidx->d);
+    delete[] reduced_data;
 }
 
 void set_ann_ef(void *ptr, int ann_ef){
